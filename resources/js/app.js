8d5ecf0cbce528bff966c7b8c633139ff533e340
peter790303 <peter790303@gmail.com>
2020-07-29 14:20:04 +0800

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import routes from './login';
 import home from './home';
 Vue.use(VueRouter);
 let path = window.location.pathname;
-path =path.replace('/project/public/','');
+path =path.replace('/','');
 let router="";
 switch (path) {
     case 'home':
